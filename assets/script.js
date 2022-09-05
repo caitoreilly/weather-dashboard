f9ef08a7f1e7d01fd56f49db5e1f2252e1233e28
Caitlin O'Reilly <coreilly29@gmail.com>
2022-09-05 17:10:54 -0400

    created layout with bootstrap, created fetch to get weather api info, display current conditions in dynamically created div

---
+++ assets/script.js
@@ -0,0 +1,73 @@
+var city = "";
+var savedCities = [];
+var fetchButton = document.getElementById("fetch-button");
+var currentDateElement = document.getElementById("current-date");
+
+// current day displayed in current search results box
+var rightNow = moment().format("MMMM Do, YYYY");
+console.log(rightNow);
+currentDateElement.textContent = rightNow;
+
+// type city and click search button --> get current conditions for city from weather API
+function getCurrent(city) {
+  var requestUrlCurrent =
+    "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=4cba12a73385abcc9d4e2c74697fadfa";
+    console.log(requestUrlCurrent);
+  fetch(requestUrlCurrent).then(function (response) {
+    return response.json();
+  }).then((data)=>{
+    console.log(data)
+    var lon = data.coord.lon;
+    var lat = data.coord.lat;
+    displayCurrent(data);
+    getForecast(lon, lat);
+  })
+}
+
+// display current conditions for city 
+function displayCurrent(data){
+    // code to display city name
+var currentCityEl = document.querySelector("#current-city");
+currentCityEl.textContent = data.name;
+// code to display weather icon
+var icon = document.querySelector("#weather-icon");
+var iconImg = document.createElement("img");
+iconImg.setAttribute("src" , "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+icon.append(iconImg);
+// code to create and display current weather info
+var weatherContainer = document.querySelector(".city-info");
+var card = document.createElement("div")
+card.setAttribute("class", "card");
+var cardBody = document.createElement("div")
+cardBody.setAttribute("class", "card-body");
+var temp = document.createElement("p")
+temp.setAttribute("class", "weatherEl");
+var humidity = document.createElement("p")
+humidity.setAttribute("class", "weatherEl");
+var wind = document.createElement("p")
+wind.setAttribute("class", "weatherEl");
+// var UV = document.createElement("p").setAttribute("class", "weatherEl");
+temp.textContent=`Temperature: ${data.main.temp}`;
+humidity.textContent=`Humidity: ${data.main.humidity} %`;
+wind.textContent=`Wind Speed: ${data.wind.speed} MPH`;
+cardBody.append(temp, humidity, wind);
+card.append(cardBody);
+weatherContainer.append(card);
+}
+
+// display future conditions for city in 5-day forecast (make 5 cards, one for each day)
+// function getForecast(lon, lat){
+// var requestUrlForecast =
+// };
+
+// city added to search history (list)
+// click on city in search history --> current & future conditions added to page again
+// UV index --> color indicates if conditions are favorable, moderate, or severe
+// future weather conditions on page --> 5 day forecast (cards) with date, icon rep of weather conditions, temp, wind speed, humidity
+
+fetchButton.addEventListener("click", (event)=>{
+    event.preventDefault()
+    var userInput = document.querySelector(".search-city").value
+    console.log(userInput);
+    getCurrent(userInput);
+});
